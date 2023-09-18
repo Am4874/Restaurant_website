2242fa22c27d72fa815360847d6fa3a04127a3e1
Amit <ambedkaramit176@gmail.com>
2023-09-18 21:15:19 +0530

    changes in navbr and some javascipt

--- javascript.js
+++ javascript.js
@@ -1,21 +1,26 @@
-let menu = document.querySelector('#menu');
-let navbar =document.querySelector('.navbar');
 
-menu.onclick = () => {
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-}
-
-var swiper = new swiper(".home-slider", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 7500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    loop:true,
+  document.addEventListener("DOMContentLoaded", function () {
+    let menu = document.querySelector('#menu');
+    let navbar = document.querySelector('.navbar');
+  
+    menu.onclick = () => {
+        menu.classList.toggle('fa-times');
+        navbar.classList.toggle('active');
+    }
+  
+    var swiper = new Swiper(".home-slider", {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+          delay: 7500,
+          disableOnInteraction: false,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        loop: true,
+    });
   });
+
+  
